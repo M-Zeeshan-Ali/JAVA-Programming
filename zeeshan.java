4d54e46a6e48f849cc212f76d53b5aa5bd745d50
ZEESHAN ALI <96174631+M-Zeeshan-Ali@users.noreply.github.com>
2022-11-21 10:10:24 +0500

    Add files via upload
    Addition Program In Java.

---
+++ zeeshan.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Scanner;
+public class zeeshan {
+
+    public static void main(String[] args)
+
+    {
+        System.out.println("****************Addition program in java***********");
+        Scanner input = new Scanner (System.in);
+        System.out.print("Input the first number: ");
+        int num1 = input.nextInt();
+        System.out.print("Input the second number: ");
+        int num2 = input.nextInt();
+        int sum = num1 + num2;
+        System.out.println();
+        System.out.println("Sum: "+sum);
+    }
+}
+
+/*
+public class AdditionProgram {
+    public static void main(String[] args){
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the 1st Number");
+        int num1=input.nextInt();
+
+        System.out.println("Enter the 2nd Number");
+        int num2=input.nextInt();
+
+        int sum;
+        sum=num1+num2;
+        System.out.println("Sum" +sum);
+    }
+}
+*/
+
+
+
+
+
+
